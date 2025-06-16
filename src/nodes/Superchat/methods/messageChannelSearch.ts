8e68167ef53c93bb8437fa8ad28de6e29d22b9c2
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2025-06-16 17:21:08 +0200

    Prefix channel options from `messageChannelSearch`

--- src/nodes/Superchat/methods/messageChannelSearch.ts
+++ src/nodes/Superchat/methods/messageChannelSearch.ts
@@ -3,9 +3,24 @@ import {
   INodeListSearchItems,
   INodeListSearchResult,
 } from "n8n-workflow";
+import { match } from "ts-pattern";
+import { ConversationType } from "../../../types/ConversationType";
 import { PageableResponse } from "../../../types/PageableResponse";
 import { PAMessageChannelConfigDTO } from "../../../types/PAMessageChannelConfigDTO";
 import { superchatJsonApiRequest } from "../GenericFunctions";
+
+function formatConversationType(input: ConversationType) {
+  return match(input)
+    .with("whats_app", () => "WhatsApp")
+    .with("google_business_messaging", () => "Google Business Messaging")
+    .with("facebook_messenger", () => "Facebook Messenger")
+    .with("telegram", () => "Telegram")
+    .with("live_chat", () => "Live Chat")
+    .with("instagram", () => "Instagram")
+    .with("sms", () => "SMS")
+    .with("mail", () => "Mail")
+    .exhaustive();
+}
 
 export async function messageChannelSearch(
   this: ILoadOptionsFunctions,
@@ -25,7 +40,7 @@ export async function messageChannelSearch(
   const results = res.results.map(
     (channel) =>
       ({
-        name: channel.name,
+        name: `${formatConversationType(channel.type)}: ${channel.name}`,
         value: channel.id,
       }) satisfies INodeListSearchItems
   );
